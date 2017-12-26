1ba9dc30f4e6ea2e842b9c58ce5c4687725f33a0
Arvind <europa1613@users.noreply.github.com>
2017-12-25 19:22:53 -0600

    Add loopylog ember 2 app

---
+++ loopylog/app/router.js
@@ -0,0 +1,15 @@
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
+
+const Router = EmberRouter.extend({
+  location: config.locationType,
+  rootURL: config.rootURL
+});
+
+Router.map(function() {
+  this.route('production', {path: "/:start/to/:end"}, function() {
+    this.route('dimension', {path: "/dimension/:dimension_id"});
+  });
+});
+
+export default Router;
